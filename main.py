3194b3eee10f2d5c6771ad71bc1ee2988f598238
AndreyT <shax0491@gmail.com>
2023-12-17 15:52:27 +0300

    Добавление файла

--- main.py
+++ main.py
@@ -9,3 +9,29 @@
 # random.shuffle(lst)
 # data = pd.DataFrame({'whoAmI':lst})
 # data.head()
+
+import pandas as pd
+import random
+
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+
+print(f"{data} \n")
+
+
+unique_values = data['whoAmI'].unique()
+
+
+for value in unique_values:
+    data[value] = (data['whoAmI'] == value).astype(int)
+
+
+data.drop('whoAmI', axis=1, inplace=True)
+
+
+data.head()
+print(data)
